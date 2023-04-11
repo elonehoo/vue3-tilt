a58c26c203616601efe7204ffa3dd3708ce0c07c
elonehoo <hi@elonehoo.me>
2023-04-11 19:57:08 +0800

    style: run eslint fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 // Import VanillaTilt
 import VanillaTilt from 'vanilla-tilt'
-import type {App} from 'vue'
+import type { App } from 'vue'
 
 // Define VueTilt-Directive
 const VueTilt = {
   install(vue: App) {
-    vue.directive('tilt', (el: HTMLElement, {value}) => {
+    vue.directive('tilt', (el: HTMLElement, { value }) => {
       VanillaTilt.init(el, Object.assign({}, value))
     })
   },
